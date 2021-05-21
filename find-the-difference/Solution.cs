7dc58c3fafa72d4454a7ffa09f18bd60893db9f1
Daniel Reis <danreis.spark@gmail.com>
2021-05-20 21:05:31 -0300

    Implement minimum viable solution

---
+++ find-the-difference/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindTheDifference
+{
+    public class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            const int charModifier = 97;
+            Span<int> lettersS = stackalloc int[26];
+            Span<int> lettersT = stackalloc int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                lettersS[s[i] - charModifier]++;
+                lettersT[t[i] - charModifier]++;
+            }
+
+            lettersT[t[^1] - charModifier]++;
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (lettersS[i] != lettersT[i]) return (char)(i + charModifier);
+            }
+
+            throw new InvalidOperationException("no new letter was added");
+        }
+    }
+}
---
+++ find-the-difference/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace FindTheDifference
+{
+    public class Tests
+    {
+        private readonly Random _rng = new Random(Environment.TickCount);
+
+        [Fact]
+        public void AddedLetterShouldBeReturned()
+        {
+            var value = "aofjqiefjoiqejfeq";
+            var shuffled = Shuffle(value, 'z');
+
+            FindTheDifference(value, shuffled).Should().Be('z');
+        }
+
+        private string Shuffle(string value, char intruder = 'a')
+            => new string(value.Append(intruder).OrderBy(v => _rng.Next()).ToArray());
+
+        private char FindTheDifference(string a, string b) => new Solution().FindTheDifference(a, b);
+    }
+}
