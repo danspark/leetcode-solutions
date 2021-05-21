8f6c5d7c00345496429e766735d81aba691ee768
Daniel Reis <danreis.spark@gmail.com>
2021-05-20 22:55:25 -0300

    Use XOR solution

--- single-number/Solution.cs
+++ single-number/Solution.cs
@@ -6,26 +6,13 @@ namespace SingleNumber
     {
         public int SingleNumber(int[] nums)
         {
-            Array.Sort(nums);
-
-            int previousNumber = nums[0];
-            int appearances = 1;
-            for (int i = 1; i < nums.Length; i++)
+            int solution = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                var currentNumber = nums[i];
-
-                if (previousNumber != currentNumber)
-                {
-                    if (appearances == 1) break;
-
-                    previousNumber = currentNumber;
-                    appearances = 0;
-                }
-
-                appearances++;
+                solution ^= nums[i];
             }
 
-            return previousNumber;
+            return solution;
         }
     }
 }
